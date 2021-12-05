82960b2c6ae172091106a20622cefd2aa9f0a59f
MrVatnik <zacvars@gmail.com>
2021-12-05 23:21:10 +0300

    added thymeleaf to project, major front changes
    search for films created.
    index page gets data from database.
    individual film page partly made.

--- src/main/java/com/example/Controller/FilmController.java
+++ src/main/java/com/example/Controller/FilmController.java
@@ -3,19 +3,48 @@ package com.example.Controller;
 import com.example.Domain.Film;
 import com.example.Service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
-@RestController
+@Controller
 public class FilmController {
     @Autowired
     private FilmService filmService;
 
-    @GetMapping("getAllFilms")
-    private List<Film> getAllFilms(){
+    @GetMapping("/")
+    private String getAllFilms(Model model){
+        List<Film> allFilms = filmService.getAllFilms();
+        model.addAttribute("films",allFilms);
+        return "index";
+    }
+
+    /*@GetMapping("getAllFilms")
+    private List<Film> getAllFilms() {
         return filmService.getAllFilms();
+    }*/
+
+    @GetMapping("filmItem")
+    private String getOneFilm(@RequestParam int id,Model model){
+        Film findFilm = filmService.getOneFilm(id).get();
+        model.addAttribute("film", findFilm);
+        return "item";
+    }
+
+    @GetMapping("/findItem")
+    private String getOneFilm(Model model){
+        model.addAttribute("film2", new Film());
+        return "searchitem";
+    }
+    @PostMapping("/findItem")
+    private String getOneFilm(@ModelAttribute Film film2, Model model){
+        Film findFilm = filmService.getFilmByTitle(film2.getTitle()).get();
+        model.addAttribute("film", findFilm);
+        return "item";
     }
 
     @PostMapping("addFilm")
@@ -30,10 +59,10 @@ public class FilmController {
         return filmService.addFilm(title, jenre, description, releaseYear, country, producerId, actorsId, ageLimit);
     }
 
-    @PostMapping("addFilm2")
+    /*@PostMapping("addFilm2")
     private Object addFilm2(@RequestBody Object test) {
         return test;
-    }
+    }*/
 
 
 }
